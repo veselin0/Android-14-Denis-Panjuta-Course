2d7b557fd04c9809e216e2a32d39afe5b5f4fec3
Veselin <vesselin60@gmail.com>
2023-11-30 16:12:28 +0200

    25. Day 2 - Int, Long, Short, Byte and specifying the type manually

---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinbasics
+
+fun main() {
+
+    var myAge: Byte = 35
+    println(myAge)
+
+    myAge = 36
+    println(myAge)
+
+    val pi = 3.14f
+    println(pi)
+}
---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/VariableTypes.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinbasics
+
+
+fun main() {
+    val constantNumber = 1
+
+    var mutableString = "hello gocho"
+
+    println(constantNumber)
+    println(mutableString)
+
+    mutableString = "Welcome Bocho"
+    println(mutableString)
+}
+
