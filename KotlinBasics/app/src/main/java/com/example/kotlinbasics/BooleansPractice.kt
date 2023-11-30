57c6c8fe4cf06e63b8cacbadd9a299211239cdaf
Veselin <vesselin60@gmail.com>
2023-11-30 16:44:55 +0200

    Start Quiz 4: Coding Exercise - Understanding Booleans in Kotlin

---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/BooleansPractice.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasics
+
+fun main() {
+    val myTrue = true
+    val myFalse = false
+
+    println(myTrue || myFalse) // This will be true
+    println(myTrue && myFalse)
+    println(!myTrue)
+}
