11aaed9db6c450e3d1d0b849a4fe3ebcb45bdf61
Veselin <vesselin60@gmail.com>
2023-12-01 14:46:09 +0200

    Initial Commit

---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/AgeControl.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinbasics
+
+fun main() {
+    println("Enter your age as a whole number: ")
+    val age = readln().toInt()
+
+    if (age in 18.. 39) println("You can enter the club.")
+//    if (age >= 18 && age < 40) println("You can enter the club.")
+    else if (age >= 40) println("You cannot go into the club, please go home.")
+    else println("Age not verified. Please contact support.")
+}
