a7e581f53f3e92be614ae93f25cb8b95c18edc93
Veselin <vesselin60@gmail.com>
2023-12-01 16:27:28 +0200

    38. Day 2 - While loops with user input

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/AgeControl.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/AgeControl.kt
@@ -1,18 +1,24 @@
 package com.example.kotlinbasics
 
 fun main() {
-//    println("Enter your age as a whole number: ")
-//    val age = readln().toInt()
-//
-//    if (age in 18.. 39) println("You can enter the club.")
-////    if (age >= 18 && age < 40) println("You can enter the club.")
-//    else if (age >= 40) println("You cannot go into the club, please go home.")
-//    else println("Age not verified. Please contact support.")
+    println("Enter your age as a whole number: ")
+    val age = readln().toInt()
+
+    if (age in 18.. 39) println("You can enter the club.")
+//    if (age >= 18 && age < 40) println("You can enter the club.")
+    else if (age >= 40) println("You cannot go into the club, please go home.")
+    else println("Age not verified. Please contact support.")
 
     var count = 0
     while (count < 3) {
         println("Count is $count")
         count++
     }
-println("Loop is done!")
+
+
+
+
+
 }
+
+
--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -2,42 +2,42 @@ package com.example.kotlinbasics
 
 fun main() {
 
-    var myAge: UShort = 35u
-    println(myAge)
-
-    myAge = 36u
-    println(myAge)
-
-    val pi = 3.14f
-    println(pi)
-
-    val myTrue: Boolean = true
-    val myFalse: Boolean = false
-
-    println(myTrue || myFalse)
-    println(myTrue && myFalse)
-    println(!myTrue)
-
-    val myChar = 'a'
-    println(myChar)
-
-    val yourChar = '\u00AE'
-    println(yourChar)
-
-    val backSlash = '\\'
-    println(backSlash)
-
-    var name = "Gocho"
-    name = "Kochev"
-    println(name)
-    println(name.uppercase())
-    println(name.lowercase())
-
-    val firstName = "Bocho"
-    val lastName = "Dochev"
-    println(firstName + " " + lastName)
-    println("$firstName $lastName")
-    println("${firstName.uppercase()} ${lastName.lowercase()}")
+//    var myAge: UShort = 35u
+//    println(myAge)
+//
+//    myAge = 36u
+//    println(myAge)
+//
+//    val pi = 3.14f
+//    println(pi)
+//
+//    val myTrue: Boolean = true
+//    val myFalse: Boolean = false
+//
+//    println(myTrue || myFalse)
+//    println(myTrue && myFalse)
+//    println(!myTrue)
+//
+//    val myChar = 'a'
+//    println(myChar)
+//
+//    val yourChar = '\u00AE'
+//    println(yourChar)
+//
+//    val backSlash = '\\'
+//    println(backSlash)
+//
+//    var name = "Gocho"
+//    name = "Kochev"
+//    println(name)
+//    println(name.uppercase())
+//    println(name.lowercase())
+//
+//    val firstName = "Bocho"
+//    val lastName = "Dochev"
+//    println(firstName + " " + lastName)
+//    println("$firstName $lastName")
+//    println("${firstName.uppercase()} ${lastName.lowercase()}")
 
 
 //    if
@@ -54,17 +54,24 @@ fun main() {
 //    }
 
 //    else if
-    var age = 0
-    println("Please, enter your age: ")
-    val inputData = readln()
-    if (inputData.isNotEmpty()) {
-        age = inputData.toInt()
-        if (age in 18 .. 39) {
-            println("You can go to the pub")
-        } else if (age >= 40) {
-            println("You can't go to the pub")
-        } else {
-            println("You are too young to get into the pub")
-        }
+//    var age = 0
+//    println("Please, enter your age: ")
+//    val inputData = readln()
+//    if (inputData.isNotEmpty()) {
+//        age = inputData.toInt()
+//        if (age in 18 .. 39) {
+//            println("You can go to the pub")
+//        } else if (age >= 40) {
+//            println("You can't go to the pub")
+//        } else {
+//            println("You are too young to get into the pub")
+//        }
+//    }
+
+    var userInput = readln()
+    while (userInput == "1") {
+        println("Loop ie executed")
+        userInput = readln()
     }
+    println("Loop is done!")
 }
