308c6913c64434ee58c91b1e3eee86479901982c
Veselin <vesselin60@gmail.com>
2023-12-04 13:45:24 +0200

    Quiz 9: Coding Exercise - Enhancing the Coffee Machine in Kotlin

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/makeCoffee.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/makeCoffee.kt
@@ -1,11 +1,18 @@
 package com.example.kotlinbasics
 
 fun main() {
-    makeCoffee(1)
-    makeCoffee(2)
-    makeCoffee(30)
+    makeCoffee("Gocho", 0)
+    makeCoffee("Bocho", 1)
+    makeCoffee("Docho", 36)
 }
 
-fun makeCoffee(sugarCount: Int, ) {
- println("coffee with $sugarCount spoons of sugar")
+fun makeCoffee(name: String, sugarCount: Int) {
+//    if (sugarCount == 1) println("coffee with $sugarCount spoon of sugar for ")
+//    else if (sugarCount == 0) println("Coffee with no sugar for $name")
+//    else println("coffee with $sugarCount spoons of sugar for $name")
+    when(sugarCount) {
+        0 -> println("Coffee with no sugar for $name.")
+        1 -> println("Coffee with $sugarCount spoon of sugar for $name.")
+        else -> println("Coffee with $sugarCount spoons of sugar for $name.")
+    }
 }
