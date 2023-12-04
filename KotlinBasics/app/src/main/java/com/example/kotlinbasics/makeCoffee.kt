6c51864ee6193f195efb7719db9b1320dd4491d1
Veselin <vesselin60@gmail.com>
2023-12-04 14:18:30 +0200

    47. Day 3 - Functions that return something

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/makeCoffee.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/makeCoffee.kt
@@ -1,18 +1,25 @@
 package com.example.kotlinbasics
 
 fun main() {
+    println(add(1, 2))
+}
+
+fun add(int1: Int, int2: Int): Int {
+    return int1 + int2
+}
+
+fun coffeeDetails() {
     println("Who is this coffee for?")
     val name = readln()
     println("How much sugar doe's the subject want?")
     val sugarCount = readln().toInt()
-    makeCoffee(name, sugarCount)
 }
 
 fun makeCoffee(name: String, sugarCount: Int) {
 //    if (sugarCount == 1) println("coffee with $sugarCount spoon of sugar for ")
 //    else if (sugarCount == 0) println("Coffee with no sugar for $name")
 //    else println("coffee with $sugarCount spoons of sugar for $name")
-    when(sugarCount) {
+    when (sugarCount) {
         0 -> println("Coffee with no sugar for $name.")
         1 -> println("Coffee with $sugarCount spoon of sugar for $name.")
         else -> println("Coffee with $sugarCount spoons of sugar for $name.")
