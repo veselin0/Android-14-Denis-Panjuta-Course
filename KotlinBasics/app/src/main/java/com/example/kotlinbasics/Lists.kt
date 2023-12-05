978b9f894db9271032ab0880c68efafb0708c3a4
Veselin <vesselin60@gmail.com>
2023-12-05 16:42:03 +0200

    61. Day 4 - Index in Lists

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Lists.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Lists.kt
@@ -5,8 +5,10 @@ fun main() {
     val shoppingList1 = listOf("Processor", "RAM", "Graphics Card", "SSD")
     val shoppingList = mutableListOf("Processor", "RAM", "Graphics Card RTX 6030", "SSD")
     shoppingList.add("Cooling System")
-    shoppingList.remove("Graphics Card RTX 6030")
-    shoppingList.add("Graphics Card RTX 4090")
+    shoppingList.removeAt(2)
+    shoppingList.removeAt(1)
+    shoppingList.add(2, "RAM")
+//    shoppingList.add("Graphics Card RTX 4090")
 
     println(shoppingList)
 }
