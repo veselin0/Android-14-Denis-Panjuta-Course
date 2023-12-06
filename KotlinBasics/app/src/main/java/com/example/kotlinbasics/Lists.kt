78898b5b6433fd16e9720ff950383a410d269dd7
Veselin <vesselin60@gmail.com>
2023-12-06 13:49:11 +0200

    64. Day 4 - Contains Method - Check if item is in the list

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Lists.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Lists.kt
@@ -4,14 +4,23 @@ fun main() {
 
     val shoppingList1 = listOf("Processor", "RAM", "Graphics Card", "SSD")
     val shoppingList = mutableListOf("Processor", "RAM", "Graphics Card RTX 6030", "SSD")
-    shoppingList.add("Cooling System")
-    shoppingList.removeAt(2)
-    shoppingList.removeAt(1)
-    shoppingList.add(2, "RAM")
-    shoppingList.add("Graphics Card RTX 4090")
-    shoppingList[4] = "Graphics Card RX 7800XT"
-    shoppingList.set(1, "Water Cooling")
-    println(shoppingList)
+
+//    shoppingList.remove("RAM")
+
+//    val hasRam = shoppingList.contains("RAM")
+
+//    if  (hasRam) println("there is RAM in the list")
+    if  (shoppingList.contains("RAM")) println("there is RAM in the list")
+    else println("there isn't RAM in the list")
+
+//    shoppingList.add("Cooling System")
+//    shoppingList.removeAt(2)
+//    shoppingList.removeAt(1)
+//    shoppingList.add(2, "RAM")
+//    shoppingList.add("Graphics Card RTX 4090")
+//    shoppingList[4] = "Graphics Card RX 7800XT"
+//    shoppingList.set(1, "Water Cooling")
+//    println(shoppingList)
 
 
 
