5589b5560632df03f510b5b5f9bccfb94e47721c
Veselin <vesselin60@gmail.com>
2023-12-06 15:41:40 +0200

    68. Day 4 - BankAccountApp - Implementing the functionality

--- BankAccountProg/app/src/main/java/com/example/bankaccountprog/BankAccount.kt
+++ BankAccountProg/app/src/main/java/com/example/bankaccountprog/BankAccount.kt
@@ -4,14 +4,23 @@ class BankAccount(var holderName: String, var balance: Double) {
     private val transactionsHistory = mutableListOf<String>()
 
     fun deposit(amount: Double) {
-
+        balance += amount
+        transactionsHistory.add("$holderName deposited $$amount")
     }
 
     fun withdraw(amount: Double) {
-
+        if (amount <= balance) {
+            balance -= amount
+            transactionsHistory.add("$holderName withdrew $$amount")
+        } else {
+            println("You have not enough funds to withdraw $$amount")
+        }
     }
 
     fun displayTransactionsHistory() {
-
+        println("Transaction History of $holderName")
+        for (transaction in transactionsHistory) {
+            println(transaction)
+        }
     }
 }
--- BankAccountProg/app/src/main/java/com/example/bankaccountprog/BankMain.kt
+++ BankAccountProg/app/src/main/java/com/example/bankaccountprog/BankMain.kt
@@ -2,4 +2,16 @@ package com.example.bankaccountprog
 
 fun main() {
 
+    val GochosBankAccount = BankAccount("Gocho Kochev", 5000.0)
+
+    GochosBankAccount.deposit(200.0)
+    GochosBankAccount.withdraw(2000.0)
+    GochosBankAccount.deposit(3000.0)
+    GochosBankAccount.deposit(2000.0)
+    GochosBankAccount.withdraw(4500.0)
+
+    GochosBankAccount.displayTransactionsHistory()
+
+    println("${GochosBankAccount.holderName}'s balance is ${GochosBankAccount.balance} ")
+
 }
