ccf9b2ced6ffb84629d7a9084e656e607753da51
Veselin <vesselin60@gmail.com>
2023-12-06 14:39:13 +0200

    66. Day 4 - Getting the Index in a for loop

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/ForLoopsWithLists.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/ForLoopsWithLists.kt
@@ -4,7 +4,7 @@ fun main() {
 
     val fruitList = mutableListOf("lemon", "apple", "kiwi", "pear", "melon")
     println(fruitList)
-    
+
     for (item in fruitList) {
         println(item)
         if (item == "kiwi") {
@@ -15,4 +15,13 @@ fun main() {
 
     println(fruitList)
 
+    println(fruitList.size)
+
+    for (index in 0..<fruitList.size) {
+        println(index)
+    }
+
+    for (index in 0..<fruitList.size) {
+        println("item ${fruitList[index]} is at index $index")
+    }
 }
