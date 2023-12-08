9f17cbf80a6e473d3f79912c871e2238c688c9e3
Veselin <vesselin60@gmail.com>
2023-12-08 14:31:52 +0200

    Play 77. Day 5 - Button, onClick and Toast

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -38,11 +41,16 @@ fun UnitConverter() {
         Text(text = "Unit Converter")
         OutlinedTextField(value = "", onValueChange = {})
         Row {
-
+            val context = LocalContext.current
+            Button(onClick = {
+                Toast.makeText(context, "Thanks for pushing me!", Toast.LENGTH_LONG).show()
+            }) {
+                Text(text = "Push me!")
+            }
         }
+        Text("Result")
     }
 }
-
 
 
 @Preview(showBackground = true)
