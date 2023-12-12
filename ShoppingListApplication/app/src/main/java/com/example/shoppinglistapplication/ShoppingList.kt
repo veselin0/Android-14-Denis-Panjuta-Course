26d8177f412087d1e52308874a76e578da56af3a
Veselin <vesselin60@gmail.com>
2023-12-12 16:44:03 +0200

    103. Day 7 - Understanding and setting up the AlertDialog

--- ShoppingListApplication/app/src/main/java/com/example/shoppinglistapplication/ShoppingList.kt
+++ ShoppingListApplication/app/src/main/java/com/example/shoppinglistapplication/ShoppingList.kt
@@ -6,26 +6,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingList() {
     val shoppingItems by remember { mutableStateOf(listOf<ShoppingListItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { showDialog = true },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(text = "Add Item")
@@ -40,4 +45,11 @@ fun ShoppingList() {
             }
         }
     }
+
+    if (showDialog) {
+        AlertDialog(onDismissRequest = { showDialog = false }) {
+            Text(text = "I am alert dialog")
+        }
+    }
+
 }
