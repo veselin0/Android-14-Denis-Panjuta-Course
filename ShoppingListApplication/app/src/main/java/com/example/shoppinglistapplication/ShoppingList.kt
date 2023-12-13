2f3caf1626a9811f03c542ed2f430f8dc72f7996
Veselin <vesselin60@gmail.com>
2023-12-13 14:49:21 +0200

    104. Day 7 - Modifying our AlertDialog and Understanding that there

--- ShoppingListApplication/app/src/main/java/com/example/shoppinglistapplication/ShoppingList.kt
+++ ShoppingListApplication/app/src/main/java/com/example/shoppinglistapplication/ShoppingList.kt
@@ -3,27 +3,33 @@ package com.example.shoppinglistapplication
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingList() {
     val shoppingItems by remember { mutableStateOf(listOf<ShoppingListItem>()) }
     var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -47,9 +53,32 @@ fun ShoppingList() {
     }
 
     if (showDialog) {
-        AlertDialog(onDismissRequest = { showDialog = false }) {
-            Text(text = "I am alert dialog")
-        }
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            confirmButton = {}, title = {
+                Text(text = "Add Shopping Item")
+            },
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = { itemName = it },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = { itemQuantity = it },
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
+            })
     }
 
 }
