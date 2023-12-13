ce39157cfbd8026f1d63bce9019b58f00b97d92f
Veselin <vesselin60@gmail.com>
2023-12-13 17:30:15 +0200

    106. Day 7 - Setting up how a listitem should look like

--- ShoppingListApplication/app/src/main/java/com/example/shoppinglistapplication/ShoppingList.kt
+++ ShoppingListApplication/app/src/main/java/com/example/shoppinglistapplication/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglistapplication
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,20 +10,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
@@ -48,7 +50,7 @@ fun ShoppingList() {
                 .padding(16.dp)
         ) {
             items(shoppingItems) {
-
+                ShoppingListItemLayout(it, {}, {})
             }
         }
     }
@@ -119,3 +121,22 @@ fun ShoppingList() {
     }
 
 }
+
+//@Composable
+//fun ShoppingListItemLayout(
+//    item: ShoppingListItem,
+//    onEditClick: () -> Unit,
+//    onDeleteClick: () -> Unit,
+//) {
+//    Row(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(8.dp)
+//            .border(
+//                border = BorderStroke(width = 2.dp, Color(0XFF018786)),
+//                shape = RoundedCornerShape(20)
+//            )
+//    ) {
+//        Text(text = item.name, modifier = Modifier.padding(8.dp))
+//    }
+//}
---
+++ ShoppingListApplication/app/src/main/java/com/example/shoppinglistapplication/ShoppingListItemLayout.kt
@@ -0,0 +1,32 @@
+package com.example.shoppinglistapplication
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShoppingListItemLayout(
+    item: ShoppingListItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(
+                border = BorderStroke(width = 2.dp, Color(0XFF018786)),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+    }
+}
