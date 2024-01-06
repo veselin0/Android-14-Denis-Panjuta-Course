007c5abb2b801d6a7c1447178944b2ba0419ec24
Veselin <vesselin60@gmail.com>
2024-01-06 16:34:24 +0200

    141. Day 9 - How each Category Item should look like

--- CookingApp/app/src/main/java/com/example/cookingapp/RecipeScreen.kt
+++ CookingApp/app/src/main/java/com/example/cookingapp/RecipeScreen.kt
@@ -1,14 +1,26 @@
 package com.example.cookingapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun RecipeScreen(modifier: Modifier = Modifier) {
@@ -25,8 +37,43 @@ fun RecipeScreen(modifier: Modifier = Modifier) {
             }
 
             else -> {
-//                display categories
+                CategoryScreen(categories = viewstate.list)
             }
         }
+    }
+}
+
+@Composable
+fun CategoryScreen(categories: List<Category>) {
+    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
+        items(categories) { category ->
+            CategoryItem(category = category)
+        }
+    }
+}
+
+// How Each item looks like
+@Composable
+fun CategoryItem(category: Category) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
+        )
+
+        Text(
+            text = category.strCategory,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 4.dp)
+        )
     }
 }
