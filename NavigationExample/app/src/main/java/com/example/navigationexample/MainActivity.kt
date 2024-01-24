7a21ee16e7f9b33a7949781a32487389b653d5af
Veselin <vesselin60@gmail.com>
2024-01-24 16:03:32 +0200

    147. Day 10 - Setting up First Screen for Navigation

---
+++ NavigationExample/app/src/main/java/com/example/navigationexample/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.navigationexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigationexample.ui.theme.NavigationExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationExampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FirstScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen() {
+    val name = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "This is the first screen", fontSize = 24.sp)
+        OutlinedTextField(value = name.value, onValueChange = { name.value = it })
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Go to Next Screen")
+        }
+    }
+}
